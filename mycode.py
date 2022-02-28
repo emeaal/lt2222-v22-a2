106c66e3b68657fecdbc3f5e9f75597ae36f14cf
emeaal <98088791+emeaal@users.noreply.github.com>
2022-02-28 16:33:46 +0100

    Update mycode.py

--- mycode.py
+++ mycode.py
@@ -1,1 +1,7 @@
-## My code
+import gzip
+
+def sample_lines(file, lines):
+    unzipped_file = gzip.open(file, "rb")
+    contents = unzipped_file.read()
+    for line in contents:
+        print(line)
